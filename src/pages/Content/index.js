d66363956af9e85abc84d79673ba7ed44f6849a9
Suket Shah <suket.shah@utexas.edu>
2023-02-10 17:35:05 -0600

    removing unecessary modules

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,6 +1,10 @@
-import { printLine } from './modules/print';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
 
 console.log('Content script works!');
 console.log('Must reload extension for modifications to take effect.');
 
-printLine("Using the 'printLine' function from the Print Module");
+const root = createRoot(document.querySelector('html'));
+root.render(<React.StrictMode></React.StrictMode>);
+
