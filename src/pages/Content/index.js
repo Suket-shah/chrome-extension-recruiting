1c76438c3b9c72318cec20b64861e290aa0b2f06
Suket Shah <suket.shah@utexas.edu>
2023-02-14 08:57:54 -0600

    working hello world screen

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import App from './modules/app';
+import Frame from 'react-frame-component';
 
 
 console.log('Content script works!');
 console.log('Must reload extension for modifications to take effect.');
 
-const root = createRoot(document.querySelector('html'));
-root.render(<React.StrictMode></React.StrictMode>);
+const frameStyle = {
+    background: "green",
+    height: "100%",
+    width: "20%",
+    position: "fixed",
+    top: "0px",
+    right: "0px",
+    zIndex: "9999",
+}
 
+const container = document.body.children[0];
+const root = createRoot(container);
+root.render(<Frame style={frameStyle}><App /></Frame>);
---
+++ src/pages/Content/modules/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import classes from './App.styles.css';
+
+const styles = {
+    color: 'blue',
+}
+function App() {
+    return (
+        <div >
+            <h1 style={styles}>Hello World</h1>
+        </div>
+    )
+}
+
+export default App;
