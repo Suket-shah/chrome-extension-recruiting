70b38f3e38997ca9c594df18712af0e85e35ce35
Suket Shah <suket.shah@utexas.edu>
2023-02-15 20:56:05 -0600

    making get request to custom search server

--- src/pages/Content/modules/App.js
+++ src/pages/Content/modules/App.js
@@ -1,13 +1,67 @@
 import React from 'react';
-import classes from './App.styles.css';
 
-const styles = {
-    color: 'blue',
-}
+import TitleModule from "./TitleModule";
+import SearchModule from "./SearchModule";
+
 function App() {
+    let lastUrl = document.location.href;
+    let lastJob = null;
+    let displayJobString = null;
+
+    const [jobPostingTitle, setJobPostingTitle] = React.useState(null);
+    const [isLoadingQuery, setIsLoadingQuery] = React.useState(false);
+    const [queryResults, setQueryResults] = React.useState(null);
+
+    function newJobPosting() {
+        console.log("New job posting, add chrome extension sidebar");
+        // todo add logic of updating iframe size and content
+    }
+
+    // setting the initial job posting state
+    if (lastUrl.includes('linkedin.com/jobs') && lastUrl.includes('currentJobId')) {
+        newJobPosting();
+    } else {
+        console.log("Not a jobs page, remove chrome extension sidebar");
+    }
+
+    // setting up the observer to watch for URL changes
+    new MutationObserver(() => {
+        const currentUrl = document.location.href;
+        const currentJob = document.querySelector('.jobs-unified-top-card__job-title');
+
+        if (currentUrl !== lastUrl) {
+            lastUrl = currentUrl;
+            if (currentUrl.includes('linkedin.com/jobs') && currentUrl.includes('currentJobId')) {
+                newJobPosting();
+            } else {
+                console.log("Not on jobs page, remove chrome extension sidebar");
+            }
+        }
+
+        if (currentJob?.innerText !== lastJob?.innerText && currentJob !== null && currentJob.innerText !== null) {
+            lastJob = currentJob;
+        } else if (currentJob === null) {
+            lastJob = null;
+        }
+
+        // jobs-unified-top-card__subtitle-primary-grouping
+        if (lastJob?.innerText !== displayJobString) {
+            displayJobString = lastJob?.innerText;
+            let displayJobAndCompany = displayJobString;
+            const locationData = document.querySelector('.jobs-unified-top-card__subtitle-primary-grouping')?.innerText;
+            if (locationData !== null && locationData !== "") {
+                console.log("document query selector" + locationData);
+                displayJobAndCompany = displayJobAndCompany + " at " + locationData;
+            }
+            setJobPostingTitle(displayJobAndCompany);
+            // TODO kick off search here and send the results to the iframe
+        }
+    }).observe(document, {subtree: true, childList: true});
+
     return (
         <div >
-            <h1 style={styles}>Hello World</h1>
+            <TitleModule title={jobPostingTitle} />
+            <SearchModule searchQuery={jobPostingTitle} setLoadingState={setIsLoadingQuery} setQueryResult={setQueryResults}/>
         </div>
     )
 }
---
+++ src/pages/Content/modules/SearchModule.js
@@ -0,0 +1,72 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const GAPIKey = "AIzaSyCA_tkBLk8QkjA0SXnhx-N-q7ryuz7uVsQ";
+const SearchCX = "50320220faa5a4aaa";
+
+function SearchModule(props) {
+
+    const queryRef = React.useRef();
+
+    function removeParentheses(query) {
+        return query.replace(/\([^)]*\)/g, '');
+    }
+
+    function removeQuotes(query) {
+        return query.replace(/"/g, '');
+    }
+
+    function removePunctuation(query) {
+        return query.replace(/[.,/#!$%^&*;:{}=\-_`~()]/g, '');
+    }
+
+    function queryCleaner(query) {
+        if (query === null) {
+            return "";
+        }
+        query = removeParentheses(query);
+        query = removeQuotes(query);
+        query = removePunctuation(query);
+        return query;
+    }
+
+    function executeQuery(event) {
+        event.preventDefault();
+        const enteredQuery = queryRef.current.value;
+        console.log("executing query", enteredQuery);
+        // TODO: generate the post request and send it to the server
+        props.setLoadingState(true);
+        searchHandler(enteredQuery);
+    }
+
+    // TODO: add error handling
+    function searchHandler(query) {
+        fetch(`https://customsearch.googleapis.com/customsearch/v1?cx=${SearchCX}&num=10&q=${encodeURIComponent(query)}&key=${GAPIKey}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        }).then((response) => {
+            return response.json();
+        }).then((jsonResponse) => {
+            console.log(jsonResponse);
+            props.setQueryResult(jsonResponse);
+            props.setLoadingState(false);
+        })
+    }
+
+    return (<div>
+        <div>
+            <form onSubmit={executeQuery}>
+                <input type="text" placeholder="EX. Google Internship AND UT" ref={queryRef} defaultValue={queryCleaner(props.searchQuery)} />
+                <button>Search</button>
+            </form>
+        </div>
+        <div>
+            <p>Powered by Linkedin X-Ray </p>
+        </div>
+        </div>
+    );
+}
+
+export default SearchModule;
