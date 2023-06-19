159294f78f7995951f8bc283dd143104904132cc
Suket Shah <suket.shah@utexas.edu>
2023-06-19 11:54:49 -0500

    Entry Button

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,4 +1,22 @@
-import { printLine } from './utils/print';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import Frame from 'react-frame-component';
 
+import { printLine } from './utils/print';
+import EntryButton from './modules/EntryButton.jsx';
+
+
+// TODO: Delete this line
 printLine("Content Script Loaded!");
 
+const documentBody = document.body;
+const recruitGptParent = document.createElement('div');
+recruitGptParent.setAttribute('id', 'recruit-gpt-root');
+documentBody.append(recruitGptParent);
+const root = createRoot(recruitGptParent);
+
+root.render(
+  <div>
+    <EntryButton />
+  </div>
+);
--- webpack.config.js
+++ webpack.config.js
@@ -156,15 +156,6 @@ var options = {
     new CopyWebpackPlugin({
       patterns: [
         {
-          from: 'src/pages/Content/content.styles.css',
-          to: path.join(__dirname, 'build'),
-          force: true,
-        },
-      ],
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
-        {
           from: 'src/assets/img/icon-128.png',
           to: path.join(__dirname, 'build'),
           force: true,
@@ -175,6 +166,15 @@ var options = {
       patterns: [
         {
           from: 'src/assets/img/icon-34.png',
+          to: path.join(__dirname, 'build'),
+          force: true,
+        },
+      ],
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'src/pages/Content/content.styles.css',
           to: path.join(__dirname, 'build'),
           force: true,
         },
